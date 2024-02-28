e508796d694e95f9a64844cea8b622464a1d79e6
Mathew <mathew@Air-de-Mathew.technicolor.net>
2024-02-28 13:43:58 +0100

    wip: added coaches and clients information in js file

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// Lista de entrenadores
+const coaches = [
+    { name: "A", reputation: 4.5, availableSlots: 1 },
+    { name: "B", reputation: 3.2, availableSlots: 4 },
+    { name: "C", reputation: 1.2, availableSlots: 3 },
+    { name: "D", reputation: 3.4, availableSlots: 2 },
+  ];
+
+// Lista de clientes
+const clients = [
+    { name: "q", importanceOfReputation: 2.6 },
+    { name: "r", importanceOfReputation: 3.7 },
+    { name: "s", importanceOfReputation: 8.5 },
+    { name: "t", importanceOfReputation: 9.7 },
+    { name: "u", importanceOfReputation: 2.6 },
+    { name: "v", importanceOfReputation: 4.7 },
+    { name: "w", importanceOfReputation: 5.6 },
+    { name: "x", importanceOfReputation: 3.7 },
+    { name: "y", importanceOfReputation: 8.1 },
+    { name: "z", importanceOfReputation: 2.5 },
+  ];
+  
